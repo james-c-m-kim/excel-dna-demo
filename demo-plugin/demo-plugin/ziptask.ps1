8eaac1ad2c93ad81e751b0463fa385fb640e1881
code4life <kimjc@code4lifesoftware.com>
2017-12-19 11:38:43 -0500

    zipping... round 1

---
+++ demo-plugin/demo-plugin/ziptask.ps1
@@ -0,0 +1,19 @@
+﻿Add-Type -assembly "system.io.compression.filesystem"
+
+$source = 'C:\dev\excel-plugin-demo\demo-plugin\demo-plugin\bin\Debug'    
+$destinationPath = "C:\dev\excel-plugin-demo\deploy"
+$destination = $destinationPath + "\excel-plugin.zip"
+
+If(Test-path $destinationPath) 
+{
+    "Cleaning up"
+    if (Test-Path $destination) {Remove-item $destination}
+}
+Else
+{
+    "Creating destination folder"
+    md $destinationPath
+}
+
+[io.compression.zipfile]::CreateFromDirectory($Source, $destination)
+"Zip complete"
