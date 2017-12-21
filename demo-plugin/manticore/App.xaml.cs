6f8dc1305111080ec615be7ec1c1e709103c983f
code4life <kimjc@code4lifesoftware.com>
2017-12-21 15:10:26 -0500

    more manticore stuff

--- demo-plugin/manticore/App.xaml.cs
+++ demo-plugin/manticore/App.xaml.cs
@@ -13,5 +13,11 @@ namespace manticore
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MessageBox.Show("test");
+            Environment.Exit(0);
+        }
     }
 }
